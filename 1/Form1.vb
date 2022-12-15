369bdce3906be783d5b0e860358ece9af750bdd5
smuglik <smuglik26@yandex.ru>
2022-12-15 19:34:34 +0300

    ïnit

---
+++ 1/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles_
+Button1.Click
+        Dim a As Single, b As Single, h As Single
+        Dim x As Single, y As Single
+        a = CSng(TextBox1.Text)
+        b = CSng(TextBox2.Text)
+        h = CSng(TextBox3.Text)
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        For x = a To b Step h
+            If x < -1.25 Then
+                y = 3 * Math.Scrt(Math.Abs(x - 1)) - 3.25
+                Elself x >= 1.25, Then
+y = 3.25 - Math.Sqrt(x + 1)
+            Else
+                y = -x
+            End If
+            TextBox4.Text = TextBox4.Text + CStr(x) + vbCrLf
+            TextBox5.Text = TextBox5.Text + CStr(y) + vbCrLf
+        Next x
+    End Sub
+
+    Private Sub Button2 _Click(sender As System.Object, e As System EventArgs) Handles_
+        Button2.Click
+        End
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ 1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("1")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("f65694d1-a14b-4415-8f49-0c84ae45292c")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
