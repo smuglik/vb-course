a1fa7d0cd95a0c836e1cb2e4403599cf47acf5a0
Smuglienko <vsmuglie@t-systems.es>
2022-12-15 21:12:09 +0100

    refactor form

--- 1/Form1.Designer.vb
+++ 1/Form1.Designer.vb
@@ -22,16 +22,82 @@ Partial Class Form1
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.Run = New System.Windows.Forms.Button()
+        Me.inputFieldA = New System.Windows.Forms.TextBox()
+        Me.inputFieldB = New System.Windows.Forms.TextBox()
+        Me.inputFieldH = New System.Windows.Forms.TextBox()
+        Me.DisplayX = New System.Windows.Forms.TextBox()
+        Me.DisplayY = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'Run
+        '
+        Me.Run.Location = New System.Drawing.Point(141, 293)
+        Me.Run.Name = "Run"
+        Me.Run.Size = New System.Drawing.Size(75, 23)
+        Me.Run.TabIndex = 0
+        Me.Run.Text = "Run"
+        Me.Run.UseVisualStyleBackColor = True
+        '
+        'inputFieldA
+        '
+        Me.inputFieldA.Location = New System.Drawing.Point(54, 144)
+        Me.inputFieldA.Name = "inputFieldA"
+        Me.inputFieldA.Size = New System.Drawing.Size(100, 20)
+        Me.inputFieldA.TabIndex = 2
+        '
+        'inputFieldB
+        '
+        Me.inputFieldB.Location = New System.Drawing.Point(175, 144)
+        Me.inputFieldB.Name = "inputFieldB"
+        Me.inputFieldB.Size = New System.Drawing.Size(100, 20)
+        Me.inputFieldB.TabIndex = 3
+        '
+        'inputFieldH
+        '
+        Me.inputFieldH.Location = New System.Drawing.Point(290, 144)
+        Me.inputFieldH.Name = "inputFieldH"
+        Me.inputFieldH.Size = New System.Drawing.Size(100, 20)
+        Me.inputFieldH.TabIndex = 4
+        '
+        'DisplayX
+        '
+        Me.DisplayX.Location = New System.Drawing.Point(531, 30)
+        Me.DisplayX.Multiline = True
+        Me.DisplayX.Name = "DisplayX"
+        Me.DisplayX.Size = New System.Drawing.Size(322, 134)
+        Me.DisplayX.TabIndex = 5
+        '
+        'DisplayY
+        '
+        Me.DisplayY.Location = New System.Drawing.Point(531, 194)
+        Me.DisplayY.Multiline = True
+        Me.DisplayY.Name = "DisplayY"
+        Me.DisplayY.Size = New System.Drawing.Size(322, 122)
+        Me.DisplayY.TabIndex = 6
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1027, 450)
+        Me.Controls.Add(Me.DisplayY)
+        Me.Controls.Add(Me.DisplayX)
+        Me.Controls.Add(Me.inputFieldH)
+        Me.Controls.Add(Me.inputFieldB)
+        Me.Controls.Add(Me.inputFieldA)
+        Me.Controls.Add(Me.Run)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Run As Button
+    Friend WithEvents inputFieldA As TextBox
+    Friend WithEvents inputFieldB As TextBox
+    Friend WithEvents inputFieldH As TextBox
+    Friend WithEvents DisplayX As TextBox
+    Friend WithEvents DisplayY As TextBox
 End Class
--- 1/Form1.vb
+++ 1/Form1.vb
@@ -1,30 +1,28 @@
 ﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class Form1
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles_
-Button1.Click
+    Private Sub CalculateValues(sender As System.Object, e As System.EventArgs) Handles Run.Click
         Dim a As Single, b As Single, h As Single
         Dim x As Single, y As Single
-        a = CSng(TextBox1.Text)
-        b = CSng(TextBox2.Text)
-        h = CSng(TextBox3.Text)
-        TextBox4.Text = ""
-        TextBox5.Text = ""
+        a = CSng(inputFieldA.Text)
+        b = CSng(inputFieldB.Text)
+        h = CSng(inputFieldH.Text)
+        DisplayX.Text = ""
+        DisplayY.Text = ""
         For x = a To b Step h
-            If x < -1.25 Then
-                y = 3 * Math.Scrt(Math.Abs(x - 1)) - 3.25
-                Elself x >= 1.25, Then
-y = 3.25 - Math.Sqrt(x + 1)
+            If x < 0 Then
+                y = Math.Pow(x, 3) - 0.3
+            ElseIf x >= 0 And x <= 1 Then
+                y = 0
             Else
-                y = -x
+                y = Math.Pow(x, 2) + x
             End If
-            TextBox4.Text = TextBox4.Text + CStr(x) + vbCrLf
-            TextBox5.Text = TextBox5.Text + CStr(y) + vbCrLf
+            DisplayX.Text = DisplayX.Text + CStr(x) + vbCrLf
+            DisplayY.Text = DisplayY.Text + CStr(y) + vbCrLf
         Next x
     End Sub
 
-    Private Sub Button2 _Click(sender As System.Object, e As System EventArgs) Handles_
-        Button2.Click
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs)
         End
     End Sub
 
@@ -33,6 +31,10 @@ y = 3.25 - Math.Sqrt(x + 1)
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles inputFieldA.TextChanged
 
     End Sub
 End Class
